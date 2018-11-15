9b977a1f001d9ae729e00e8be369dec1f2bf0410
lcha105u <lcha105u@gmail.com>
2018-11-16 02:35:10 +0800

    clean trash

--- server.c
+++ server.c
@@ -1,111 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <signal.h>
-#include <time.h>
 
-#define Port_Num 8099
+#define PORT_NUM 8000
+#define BUFFER_SIZE 4096
+#define BACKLOG_NUM 5
 
-void write_protocol(char *http_header);
+int getSocketFD(void);
+
+void write_Protocol(char *);
 char *get_time(char *);
+int get_Request(char *response);
+
+void init_Server(struct sockaddr_in *);
+void bind_Server(int, struct sockaddr_in *);
+void listen_Server(int);
+void signal_Action(int ,struct sigaction *);
+
+int accept_Client(int, struct sockaddr_in *);
+void socket_Handler(int, char *, int);
 
 static void wait_child(int singNum){
 	while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-void socket_handler(int socket_fd, char *recieve, char *response, int length){
-	send(socket_fd, response, length, 0);
-	recv(socket_fd, recieve, 1024, 0);
-	close(socket_fd);
-}
-
 int main(int argc, char *argv[]){
-	
-	//open html file
-	int htmlFD = open("index.html", O_RDONLY);
-	char htmlContent[4096];
-	char recieve[1024];
-	int length = read(htmlFD, htmlContent, 4096);
-	
-	htmlContent[length] = '\0';
-
-	char response[2048];
-	write_protocol(response);
-
-	length += strlen(response);
-	strcat(response, htmlContent);
-
-	int network_socket, new_socket, pid;
+	int serverSocket, clientSocket;
+	int replyLength = 0;
+	char recieve[BUFFER_SIZE], response[BUFFER_SIZE];
+	struct sockaddr_in serverInfo, clientInfo;
 	struct sigaction s;
 
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	serverSocket = getSocketFD();
 
-	if(network_socket == -1){
-		perror("socket error");
-	}
+	replyLength = get_Request(response);
+	
+	init_Server(&serverInfo);
 
-	struct sockaddr_in serverInfo, clientInfo;
-	int client_addrlen = sizeof(clientInfo);
-	int status;	// waiting for child process to complete
+	bind_Server(serverSocket, &serverInfo);
 
-	serverInfo.sin_family = AF_INET;
-	serverInfo.sin_addr.s_addr = INADDR_ANY;
-	serverInfo.sin_port = htons(Port_Num);
+	listen_Server(serverSocket);
 
-	if(bind(network_socket, (struct sockaddr *) &serverInfo, sizeof(serverInfo)) == -1){
-		perror("bind error");
-	}
-
-	if(listen(network_socket, 5) == -1){
-		perror("listen error");
-	}
-
-	s.sa_handler = wait_child;
-	sigemptyset(&s.sa_mask);
-	s.sa_flags = SA_RESTART;
-	if(sigaction(SIGCHLD, &s, NULL) == -1){
-		perror("sigaction error");
-	}
-
+	signal_Action(serverSocket, &s);
 
 	while(1){
-		// get accepted socket
-		new_socket = accept(network_socket, (struct sockaddr *) &clientInfo, (unsigned int *) &client_addrlen);
-		if(new_socket == -1){
-			perror("client_socket error");
-			exit(EXIT_FAILURE);
-		}
-		socket_handler(new_socket, recieve, response, length);
-		fprintf(stderr, "execute\n");
-		// do fork
+		int pid;
+
+		clientSocket = accept_Client(serverSocket, &clientInfo);
+
 		pid = fork();
+
 		if(pid < 0){
 			perror("fork error");
+			close(clientSocket);
+			close(serverSocket);
+			exit(EXIT_FAILURE);
 		}
-		else if(pid == 0){	// child process
-			//close(network_socket);
-			socket_handler(new_socket, recieve, response, length);
+
+		else if(pid == 0){
+			socket_Handler(clientSocket, response, replyLength);
 		}
-		else{			// parent process
-			//close(new_socket);
-			break;
-		}
-		
+
+		close(clientSocket);
 	}
 
-	close(network_socket);
-	printf("done\n");
+	close(serverSocket);
 	return 0;
 }
 
+int getSocketFD(void){
+	int socketFD;
+
+	socketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+	if(socketFD == -1){
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
+	return socketFD;
+}
+
 char *get_time(char *c_time_string){
+
 	time_t current_time;
 
 	current_time = time(NULL);
@@ -125,16 +110,86 @@ char *get_time(char *c_time_string){
 	return c_time_string;
 }
 
-void write_protocol(char *http_header){
+void write_Protocol(char *http_header){
 	char header[] = "HTTP/1.1 200 OK\r\n";
 	char host[] = "Host:localhost\r\n";
-	char port[] = "Port:8001\r\n";
+	char port[] = "Port:";
 	char time[512];
 	char *time_Reg = get_time(time);
 
-	strcat(http_header, header);
-	strcat(http_header, host);
-	//strcat(http_header, Port_Num);
-	strcat(http_header, time_Reg);
-	strcat(http_header, "\r\n\n");
+	sprintf(http_header, "%s%s%s%d\r\n%s\r\n\n", header, host, port, PORT_NUM, time_Reg);
+}
+
+int get_Request(char *response){
+	int htmlFD = open("index.html", O_RDONLY);
+	char htmlContent[BUFFER_SIZE];
+	int length = read(htmlFD, htmlContent, BUFFER_SIZE);
+
+	htmlContent[length] = '\0';
+	write_Protocol(response);
+
+	length += strlen(response);
+	strcat(response, htmlContent);
+
+	return length;
+}
+
+void init_Server(struct sockaddr_in *serverInfo){
+
+	serverInfo->sin_family = AF_INET;
+	serverInfo->sin_addr.s_addr = INADDR_ANY;
+	serverInfo->sin_port = htons(PORT_NUM);
+}
+
+void bind_Server(int socketFD, struct sockaddr_in *serverInfo){
+
+	if(bind(socketFD, (struct sockaddr *) serverInfo, sizeof(struct sockaddr_in)) == -1){
+		perror("bind");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void listen_Server(int socketFD){
+
+	if(listen(socketFD, BACKLOG_NUM) == -1){
+		perror("listen");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void signal_Action(int socketFD, struct sigaction *s){
+	
+	s->sa_handler = wait_child;
+	sigemptyset(& (s->sa_mask));
+	s->sa_flags = SA_RESTART;
+
+	if(sigaction(SIGCHLD, s, NULL) == -1){
+		perror("sigaction");
+		close(socketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+int accept_Client(int serverSocketFD, struct sockaddr_in *clientInfo){
+	
+	int clientSocketFD;
+	socklen_t length = sizeof(struct sockaddr_in);
+
+	clientSocketFD = accept(serverSocketFD, (struct sockaddr *) clientInfo, &length);
+
+	if(clientSocketFD == -1){
+		perror("accept");
+		close(serverSocketFD);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void socket_Handler(int socketFD, char *response, int length){
+
+	char recieve[BUFFER_SIZE];
+
+	send(socketFD, response, length, 0);
+	recv(socketFD, recieve, BUFFER_SIZE, 0);
 }
